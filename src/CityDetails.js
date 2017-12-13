4a5d2bc24775f2e7a600941582614d955ea52b5a
brbelic <br.belic@gmail.com>
2017-12-13 16:45:55 +0100

    Changed background and some of the divs

--- src/CityDetails.js
+++ src/CityDetails.js
@@ -68,46 +68,44 @@ class CityDetails extends Component {
             return <div>The response it not here yet!</div>;
         }
         return (
-            <div className="App">
-                <div className="App-header outer">
-                    <div className="inner container">
-                        <div className="row">
-                            <div className="appTitle col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-ms-offset-2">
-                                <h2>Weather App</h2>
-                            </div>
-                            <button className="metric-button col-xs-2 col-sm-2 col-md-2" onClick={this.handleClick}>
-                                C/F/K
-                            </button>
+            <div className="main-body">
+                <div className="container">
+                    <div className="row">
+                        <div className="appTitle col-xs-8 col-xs-offset-2 col-sm-8 col-sm-offset-2 col-md-8 col-ms-offset-2">
+                            <h2>Weather App</h2>
                         </div>
-                        <br />
-                        <div className="cityDetails">
-                            <div className="cityName row">
-                                <span>{this.state.city.name.toUpperCase()}</span>
-                            </div>
-                            <div className="allDays container">
-                                <div className="daysHeading row">
-                                    <div className="date col-xs-3 col-sm-3 col-md-3">
-                                        <span>Date</span>
-                                    </div>
-                                    <div className="temp col-xs-2 col-sm-2 col-md-2">
-                                        <span>Temp</span>
-                                    </div>
-                                    <div className="temp-min col-xs-2 col-sm-2 col-md-2">
-                                        <span>Min. temp</span>
-                                    </div>
-                                    <div className="temp-max col-xs-2 col-sm-2 col-md-2">
-                                        <span>Max. temp</span>
-                                    </div>
-                                    <div className="description col-xs-3 col-sm-3 col-md-3">
-                                        <span>Description</span>
-                                    </div>
+                        <button className="metric-button col-xs-2 col-sm-2 col-md-2" onClick={this.handleClick}>
+                            C/F/K
+                        </button>
+                    </div>
+                    <br />
+                    <div className="cityDetails">
+                        <div className="cityName">
+                            <h3>{this.state.city.name.toUpperCase()}</h3>
+                        </div>
+                        <div className="allDays container">
+                            <div className="daysHeading row">
+                                <div className="date col-xs-3 col-sm-3 col-md-3">
+                                    <span>Date</span>
                                 </div>
-                                <Day cityDetails={this.state.cityDetails} unit={this.state.unit}/>
-                            </div>
-                            <div className="row">
-                                <div className="col-xs-10 col-xs-offset-1 map-col">
-                                    <Location center={this.state.city.coord} />
+                                <div className="temp col-xs-2 col-sm-2 col-md-2">
+                                    <span>Temp</span>
                                 </div>
+                                <div className="temp-min col-xs-2 col-sm-2 col-md-2">
+                                    <span>Min. temp</span>
+                                </div>
+                                <div className="temp-max col-xs-2 col-sm-2 col-md-2">
+                                    <span>Max. temp</span>
+                                </div>
+                                <div className="description col-xs-3 col-sm-3 col-md-3">
+                                    <span>Description</span>
+                                </div>
+                            </div>
+                            <Day cityDetails={this.state.cityDetails} unit={this.state.unit}/>
+                        </div>
+                        <div className="row map-container">
+                            <div className="col-xs-10 col-xs-offset-1 map-col">
+                                <Location center={this.state.city.coord} />
                             </div>
                         </div>
                     </div>
